4e9a8fd4e536d3dc67645904d7531cb42d718b21
Soumya <101737464+kazumi513@users.noreply.github.com>
2025-04-16 11:27:34 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -87,13 +87,13 @@ function changeChannel(videoId) {
 }
 
 const channels = [
-  'M-4zE2GG87w', 'vYIYIVmOo3Q', 'CX9_h23icoM', 'yf5NOyy1SXU', '4LIu4EyuDXI', 'nP-aOc7g228', '5D5b0-hfzno', 'jEIFHt4q6nA', 'IDZHHQsmvmc',
-  'yr9ZxQaWkqs', '_mHmfLfx0NU', 'x0OLPEjna8A', 'KMXZF-K2mus', 'GB7kh1tvZxY', 'orFvdB0gJng', 'XM8bbRA3qio', 'd_t5nnK9Rn4', '5yx6BWlEVcY',
-  'LC0fYEpy2ng', '0pb3E4PGxq8', 'Vg13S-zzol0', 'FYJ7RCDgFwE', 'vrB9wC6quaU', 'hB2LatX6NLg', '92PvEVG0sKI', 'r3JG5gBLbpA', '5qap5aO4i9A',
-  '28KRPhVzCus', 'ft6R-UNXLSs', 'snL_Pdh51ww', 'dw_Bx0e0lis', 'pxWfp8Hxazo', 'x9MolslcpzU', '1AOMHYL_pz4', 'jfKfPfyJRdk', 'R4jvhTed_gM',
-  'WcI5cDt37ec', '7NOSDKb0HlU', 'PRgS7hBgR1k', 'Lv0SzPoZltY', '6tWWPn1lYgU', 'erUTqlcsDJI', 'Na0w3Mz46GA', 'wJSg1H8wOUg', 'pJE-euPdroo',
-  'dxyaNvclgE8'
-];
+'M-4zE2GG87w', 'r3JG5gBLbpA', 'vrB9wC6quaU', '92PvEVG0sKI', 'hB2LatX6NLg','vYIYIVmOo3Q', 'CX9_h23icoM', 'yf5NOyy1SXU',
+'4LIu4EyuDXI', '5yx6BWlEVcY', '0pb3E4PGxq8','Vg13S-zzol0','FYJ7RCDgFwE', 'LC0fYEpy2ng', 'nP-aOc7g228', '5D5b0-hfzno',
+'jEIFHt4q6nA', 'IDZHHQsmvmc','yr9ZxQaWkqs', '_mHmfLfx0NU', 'x0OLPEjna8A', 'KMXZF-K2mus', 'GB7kh1tvZxY', 'orFvdB0gJng',
+'XM8bbRA3qio', 'd_t5nnK9Rn4', '28KRPhVzCus', 'ft6R-UNXLSs', 'snL_Pdh51ww', 'dw_Bx0e0lis','pxWfp8Hxazo','x9MolslcpzU',
+'1AOMHYL_pz4','jfKfPfyJRdk', 'R4jvhTed_gM', 'WcI5cDt37ec', '7NOSDKb0HlU','PRgS7hBgR1k','Lv0SzPoZltY', '6tWWPn1lYgU', 
+'erUTqlcsDJI', 'Na0w3Mz46GA', 'wJSg1H8wOUg', 'pJE-euPdroo' ]
+
 
 function surpriseChannel() {
   changeChannel(channels[Math.floor(Math.random() * channels.length)]);
